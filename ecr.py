a326eef668347242a088b1600b19ff3db6cb57ba
“v3nom007” <shivam.suryas5@gmail.com>
2024-03-26 23:02:16 +0530

    push code

---
+++ ecr.py
@@ -0,0 +1,11 @@
+import boto3
+
+ecr_client = boto3.client('ecr')
+
+repository_name = 'todo-ecr'
+
+response =  ecr_client.create_repository(repositoryName=repository_name)
+
+repository_uri = response['repository']['repositoryUri']
+
+print(repository_uri)
